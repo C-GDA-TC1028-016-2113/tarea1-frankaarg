39cf6ffeae4c5cedffa3789cabb9cb020e4de466
frankaarg <a01635790@itesm.mx>
2021-08-19 00:50:35 +0000

    José Francisco Arias Gómez A01635790

--- assignments/06Peso/src/exercise.py
+++ assignments/06Peso/src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
+    peso1 = float(input("Dame el peso inicial: "))
+    peso2 = float(input("Dame el peso final: "))
+    meses = int(input("Dame la cantidad de meses: "))
+    float(int(meses))
+    peso3 = (peso1 - peso2)/meses
+    print("Lo que debes bajar por mes es:", peso3)
+
     pass
 
 
